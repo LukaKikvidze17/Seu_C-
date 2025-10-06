4669afa947f886900d5b11ff57c70ce66e039c59
LukaKikvidze <programming1725@gmail.com>
2025-10-06 21:54:15 +0400

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("y = ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.Write("x = ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(y = x + 1 + Math.Pow(x,2) / 2);
+        }
+    }
+};
+
