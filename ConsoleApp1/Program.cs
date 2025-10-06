6fa6f80d1e3f720727f62b0be30da67427db8705
LukaKikvidze <programming1725@gmail.com>
2025-10-06 22:14:15 +0400

    2.1.3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,10 +9,8 @@ namespace MyNamespace
         {
             Console.Write("x = ");
             double x = Convert.ToDouble(Console.ReadLine());
-            Console.Write("z = ");
-            double z = Convert.ToDouble(Console.ReadLine());
-            double y = (Math.Pow(x, 2) + Math.Pow(z, 2)) / (2 * (1 - (Math.Pow(x, 2) - Math.Pow(z, 2))));
-            Console.WriteLine(y);
+            double y = Math.Sqrt(1 + Math.Sqrt(Math.Abs(x)));
+            Console.WriteLine("y == " + y);
         }
     }
 }; 
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4669afa947f886900d5b11ff57c70ce66e039c59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cc4e64e8cafd3369bfac7311ab144a17d88b545")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
